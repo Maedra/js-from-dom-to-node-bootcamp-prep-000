5bc1c34d052a56719decdf94cbf52797882060d3
Amanda K <kosateam@yahoo.com>
2017-10-28 03:55:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ console.log("Hello, World")
 if (6+3 === 9){
   console.log("Giraffes have no vocal cords.")
 }
+
+if (6+3<5) {
+  console.log("The hummingbird is the only animal that can fly backwards")
+}
